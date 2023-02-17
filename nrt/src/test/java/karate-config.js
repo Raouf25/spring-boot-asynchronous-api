3fca1a9234f7a0dfadb8f679f7538c7e147863a8
Raouf MAKHLOUF <makhlouf.raouf@gmail.com>
2023-02-17 07:27:54 +0100

    feat: fly deployment with workflow

--- nrt/src/test/java/karate-config.js
+++ nrt/src/test/java/karate-config.js
@@ -1,7 +1,8 @@
 function init() {
 
     var config = {
-        baseUrl : 'http://localhost:8080'
+        // baseUrl : 'http://localhost:8080'
+        baseUrl : 'http://spring-boot-asynchronous-api.fly.dev'
     };
 
     return config;
