41e27f663a24694f6492b0a70a01d96170c643a3
Raouf MAKHLOUF <makhlouf.raouf@gmail.com>
2023-02-17 07:17:26 +0100

    run application with h2 data base

---
+++ app/src/main/resources/data.sql
@@ -0,0 +1,17 @@
+INSERT INTO player(name, age, club, nationality)
+VALUES ('Son Heung-Min', 29, 'Tottenham Hotspur', 'South-Korea');
+
+INSERT INTO player(name, age, club, nationality)
+VALUES ('Mohamed Salah', 29, 'Liverpool', 'Egypt');
+
+INSERT INTO player(name, age, club, nationality)
+VALUES ('Teemu Pukki', 31, 'Norwich City', 'Finland');
+
+INSERT INTO player(name, age, club, nationality)
+VALUES ('Ivan Toney', 25, 'Brentford', 'England');
+
+INSERT INTO player(name, age, club, nationality)
+VALUES ('Harry Kane', 32, 'Tottenham Hotspur', 'England');
+
+INSERT INTO player(name, age, club, nationality)
+VALUES ('Mason Mount', 22, 'Chelsea', 'England');
