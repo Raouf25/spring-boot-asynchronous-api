39c77cdb4123a28f52dfe879d89146f5ac06d6c1
Raouf MAKHLOUF <makhlouf.raouf@gmail.com>
2023-04-03 04:35:09 +0200

    fix: karate tests

--- nrt/src/test/java/features/SuiteTest.java
+++ nrt/src/test/java/features/SuiteTest.java
@@ -2,13 +2,11 @@ package features;
 
 import com.intuit.karate.Results;
 import com.intuit.karate.Runner;
-import cucumber.api.CucumberOptions;
 import org.junit.jupiter.api.Test;
 
 import static  org.junit.jupiter.api.Assertions.assertEquals;
 
 
-@CucumberOptions(tags = {"~ignore"})
 class SuiteTest {
 
     @Test
